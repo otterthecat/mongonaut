2188aeee9e96a3ee7e3d1ca8b99be2dfa9e46faa
Otter <otc@otterthecat.com>
2016-01-10 15:33:00 -0600

    update query spec to use ES6 template strings

--- test/specs/querySpec.js
+++ test/specs/querySpec.js
@@ -3,7 +3,6 @@
 // assertion library
 // /////////////////////////////////////////////////////////
 var chai = require('chai');
-var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
 chai.should();
 chai.use(sinonChai);
@@ -23,13 +22,13 @@ let fakeTarget = 'fake.json';
 let query = require('../../lib/query');
 
 describe('query', function () {
-  it('should generate query string', function (){
+  it('should generate query string', function () {
     let returnValue = query.call(configMock, fakeTarget);
-    returnValue.should.contain('-db ' + configMock.db);
-    returnValue.should.contain('-u ' + configMock.user);
-    returnValue.should.contain('-p ' + configMock.pwd);
-    returnValue.should.contain('--authenticationDatabase', + configMock.db);
-    returnValue.should.contain('--collection ' + configMock.collection);
-    returnValue.should.contain(fakeTarget + ' --jsonArray');
+    returnValue.should.contain(`-db ${configMock.db}`);
+    returnValue.should.contain(`-u ${configMock.user}`);
+    returnValue.should.contain(`-p ${configMock.pwd}`);
+    returnValue.should.contain(`--authenticationDatabase ${configMock.db}`);
+    returnValue.should.contain(`--collection ${configMock.collection}`);
+    returnValue.should.contain(`${fakeTarget} --jsonArray`);
   });
 });
