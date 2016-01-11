3c18324ec72c21d965e1de2398ce335b9d038305
Otter <otc@otterthecat.com>
2016-01-10 22:43:48 -0600

    corrected order of arguments for Promise (doh)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let Mongonaut = function (options) {
 
 Mongonaut.prototype = {
   'import': function (target) {
-    return new Promise ((reject, resolve) => {
+    return new Promise ((resolve, reject) => {
       this.exec(this.query(target), (err, stdout, stderr) => {
         if (err) {
           reject(err);
