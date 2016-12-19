9d704464d1aa91c7d6c92234c9d414f225926692
Otter <otc@otterthecat.com>
2016-12-19 16:52:20 -0500

    Write logic for .set() function. Refactor .import() and .export()

--- index.js
+++ index.js
@@ -11,15 +11,24 @@ let Mongonaut = function (options) {
 
 Mongonaut.prototype = {
   'import': function (file) {
-    return SPAWN('mongoimport', QUERY['import'](this.config).split(' '))
+    let options = Object.assign({}, this.config, {'file': file})
+    return SPAWN('mongoimport', QUERY['import'](options).split(' '))
   },
 
   'export': function (collection) {
+    if (collection) this.config.collection = collection
     return SPAWN('mongoexport', QUERY['export'](this.config).split(' '))
   },
 
-  'set': function () {
-
+  'set': function (...args) {
+    if (args.length === 1 && typeof args === 'object') {
+      this.config = Object.assign(this.config, args)
+      return this
+    } else if (args.length === 2 && typeof args[0] === 'string' && typeof args[0] === 'string') {
+      this.config[args[0]] = args[1]
+      return this
+    }
+    return new Error('Invalid argument(s) passed to .set()')
   }
 }
 
