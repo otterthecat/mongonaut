bf78c29c4f3023a2bb5eb364cad5724d37b3aa59
Otter <otc@otterthecat.com>
2016-12-23 15:30:23 -0500

    add tests for import/export functions

--- test/mongonautSpec.js
+++ test/mongonautSpec.js
@@ -1,8 +1,24 @@
 'use strict'
 
 const test = require('ava')
+const cp = require('child_process')
+const sinon = require('sinon')
+
+sinon.stub(cp, 'spawn', function (action, opts) {
+  return [action, opts]
+})
 const Mongonaut = require('../index')
 let m = new Mongonaut()
+
+test('import() should spawn a new mongoimport process', function (t) {
+  let stubReturn = m.import('foo.json')
+  t.is(stubReturn[0], 'mongoimport')
+})
+
+test('export() should spawn a new mongoexport process', function (t) {
+  let stubReturn = m.export('foo')
+  t.is(stubReturn[0], 'mongoexport')
+})
 
 test('set() should update config when passed an object ', function (t) {
   m.set({user: 'foo', pwd: 'bar'})
@@ -15,4 +31,9 @@ test('set() should update config when passed key/value strings', function (t) {
   m.set('collection', 'meh')
 
   t.is(m.config.collection, 'meh')
+})
+
+test('set() should return an error if passed invalid data', function (t) {
+  var returnValue = m.set()
+  t.is(returnValue instanceof Error, true)
 })
