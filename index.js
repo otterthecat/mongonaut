13346642fa9879774dd820b05047bd24d6ab2945
Otter <otc@otterthecat.com>
2016-12-26 15:43:38 -0500

    extract current code into own codebase (re: spawngo). Refactor Mongonaut to essentially be a promise wrapper. Update tests & dependencies

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 'use strict'
 
-const SPAWN = require('child_process').spawn
-const DEFAULTS = require('./lib/defaults')()
-const QUERY = require('./lib/query')
+const Spawngo = require('spawngo')
+const PROMISIFY = require('./lib/helpers').promisify
 
-let Mongonaut = function (options) {
-  this.config = Object.assign(DEFAULTS, options)
-  this.allowedFileTypes = new Set(['json', 'csv', 'tsv'])
-}
+class Mongonaut extends Spawngo {
+  import (file) {
+    return PROMISIFY(super.import(file))
+  }
 
-Mongonaut.prototype = {
-  'import': function (file) {
-    let options = Object.assign({}, this.config, {'file': file})
-    return SPAWN('mongoimport', QUERY['import'](options).split(' '))
-  },
-
-  'export': function (collection) {
-    if (collection) this.config.collection = collection
-    return SPAWN('mongoexport', QUERY['export'](this.config).split(' '))
-  },
-
-  'set': function (...args) {
-    if (args.length === 1 && typeof args[0] === 'object') {
-      this.config = Object.assign(this.config, args[0])
-      return this
-    } else if (args.length === 2 && typeof args[0] === 'string' && typeof args[0] === 'string') {
-      this.config[args[0]] = args[1]
-      return this
-    }
-    return new Error('Invalid argument(s) passed to .set()')
+  export (collection) {
+    return PROMISIFY(super.export(collection))
   }
 }
 
